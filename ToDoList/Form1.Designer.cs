237b455a336405b867d100bf3ad61b8203331ce3
xodml <xodml1237@gmail.com>
2025-05-06 21:11:27 +0900

    ToDoList Main Structure
    전체적인 투두리스트 구조
    일정 추가, 수정, 삭제
    달력으로 날짜 지정
    테이블에 전체 데이터 및 체크박스

---
+++ ToDoList/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace ToDoList
+{
+    partial class ToDoList
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.titleTextBox = new System.Windows.Forms.TextBox();
+            this.descriptionTextBox = new System.Windows.Forms.TextBox();
+            this.Description = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.editButton = new System.Windows.Forms.Button();
+            this.deleteButton = new System.Windows.Forms.Button();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.toDoListView = new System.Windows.Forms.DataGridView();
+            this.timerSliding = new System.Windows.Forms.Timer(this.components);
+            this.panel = new System.Windows.Forms.Panel();
+            this.calendar = new System.Windows.Forms.MonthCalendar();
+            ((System.ComponentModel.ISupportInitialize)(this.toDoListView)).BeginInit();
+            this.panel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("굴림", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.Location = new System.Drawing.Point(412, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(1064, 149);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "To Do List";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // titleTextBox
+            // 
+            this.titleTextBox.Location = new System.Drawing.Point(408, 186);
+            this.titleTextBox.Name = "titleTextBox";
+            this.titleTextBox.Size = new System.Drawing.Size(1068, 35);
+            this.titleTextBox.TabIndex = 1;
+            // 
+            // descriptionTextBox
+            // 
+            this.descriptionTextBox.Location = new System.Drawing.Point(408, 256);
+            this.descriptionTextBox.Name = "descriptionTextBox";
+            this.descriptionTextBox.Size = new System.Drawing.Size(1068, 35);
+            this.descriptionTextBox.TabIndex = 2;
+            // 
+            // Description
+            // 
+            this.Description.Location = new System.Drawing.Point(408, 224);
+            this.Description.Name = "Description";
+            this.Description.Size = new System.Drawing.Size(1068, 23);
+            this.Description.TabIndex = 3;
+            this.Description.Text = "Description:";
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(408, 158);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(1068, 23);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Title:";
+            // 
+            // editButton
+            // 
+            this.editButton.Location = new System.Drawing.Point(408, 297);
+            this.editButton.Name = "editButton";
+            this.editButton.Size = new System.Drawing.Size(335, 64);
+            this.editButton.TabIndex = 6;
+            this.editButton.Text = "Edit";
+            this.editButton.UseVisualStyleBackColor = true;
+            this.editButton.Click += new System.EventHandler(this.editButton_Click);
+            // 
+            // deleteButton
+            // 
+            this.deleteButton.Location = new System.Drawing.Point(749, 297);
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.Size = new System.Drawing.Size(362, 64);
+            this.deleteButton.TabIndex = 7;
+            this.deleteButton.Text = "Delete";
+            this.deleteButton.UseVisualStyleBackColor = true;
+            this.deleteButton.Click += new System.EventHandler(this.deleteButton_Click);
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(1117, 297);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(362, 64);
+            this.saveButton.TabIndex = 8;
+            this.saveButton.Text = "Save";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // toDoListView
+            // 
+            this.toDoListView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.toDoListView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.toDoListView.Location = new System.Drawing.Point(408, 367);
+            this.toDoListView.Name = "toDoListView";
+            this.toDoListView.RowHeadersWidth = 82;
+            this.toDoListView.RowTemplate.Height = 37;
+            this.toDoListView.Size = new System.Drawing.Size(1071, 577);
+            this.toDoListView.TabIndex = 9;
+            this.toDoListView.CellBeginEdit += new System.Windows.Forms.DataGridViewCellCancelEventHandler(this.toDoListView_CellBeginEdit);
+            // 
+            // timerSliding
+            // 
+            this.timerSliding.Interval = 10;
+            // 
+            // panel
+            // 
+            this.panel.Controls.Add(this.calendar);
+            this.panel.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel.Location = new System.Drawing.Point(0, 0);
+            this.panel.Name = "panel";
+            this.panel.Size = new System.Drawing.Size(402, 1048);
+            this.panel.TabIndex = 10;
+            // 
+            // calendar
+            // 
+            this.calendar.Location = new System.Drawing.Point(9, 9);
+            this.calendar.Name = "calendar";
+            this.calendar.TabIndex = 0;
+            // 
+            // ToDoList
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1575, 1048);
+            this.Controls.Add(this.panel);
+            this.Controls.Add(this.toDoListView);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.deleteButton);
+            this.Controls.Add(this.editButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Description);
+            this.Controls.Add(this.descriptionTextBox);
+            this.Controls.Add(this.titleTextBox);
+            this.Controls.Add(this.label1);
+            this.Name = "ToDoList";
+            this.Text = "To Do List";
+            this.Load += new System.EventHandler(this.ToDoList_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.toDoListView)).EndInit();
+            this.panel.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox titleTextBox;
+        private System.Windows.Forms.TextBox descriptionTextBox;
+        private System.Windows.Forms.Label Description;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button editButton;
+        private System.Windows.Forms.Button deleteButton;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.DataGridView toDoListView;
+        private System.Windows.Forms.Timer timerSliding;
+        private System.Windows.Forms.Panel panel;
+        private System.Windows.Forms.MonthCalendar calendar;
+    }
+}
+
---
+++ ToDoList/Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ToDoList
+{
+    public partial class ToDoList : Form
+    {
+        public ToDoList()
+        {
+            InitializeComponent();
+        }
+
+        //데이터를 데이터 테이블에 저장 후 데이터 그리드 뷰에서 볼 수 있도록 함
+        DataTable todoList = new DataTable();
+        //현재 데이터를 수정 중인가 
+        bool isEditing = false;
+
+        private void ToDoList_Load(object sender, EventArgs e)
+        {
+            //제목, 설명, 시작날짜, 종료날짜 추가
+            todoList.Columns.Add("Title");
+            todoList.Columns.Add("Description");
+            todoList.Columns.Add("Start", typeof(DateTime));
+            todoList.Columns.Add("End",typeof(DateTime));
+
+            //체크박스 추가
+            DataGridViewCheckBoxColumn checkBoxColumn = new DataGridViewCheckBoxColumn();
+            checkBoxColumn.HeaderText = "완료 여부";
+            checkBoxColumn.Name = "IsCompleted";
+            toDoListView.Columns.Add(checkBoxColumn);
+
+            toDoListView.AllowUserToAddRows = false;
+
+            toDoListView.DataSource = todoList;
+
+            //체크박스 열의 너비 수정, 그리드에 데이터를 넣은 후에 너비 설정
+            toDoListView.Columns[0].Width = 100;
+            toDoListView.RowHeadersWidth = 80;
+        }
+
+        private void editButton_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                //셀을 선택하지 않았거나 항목이 존재하지 않는 경우
+                if (toDoListView.CurrentCell == null || toDoListView.CurrentCell.RowIndex < 0)
+                {
+                    MessageBox.Show("수정할 항목을 먼저 선택하세요.");
+                    return;
+                }
+
+                isEditing = true;
+
+                //선택된 셀들을 텍스트 박스로 올림
+                titleTextBox.Text = todoList.Rows[toDoListView.CurrentCell.RowIndex].ItemArray[0].ToString();
+                descriptionTextBox.Text = todoList.Rows[toDoListView.CurrentCell.RowIndex].ItemArray[1].ToString();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Error: " + ex);
+            }
+        }
+
+        private void deleteButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //셀을 선택하지 않았거나 항목이 존재하지 않는 경우
+                if (toDoListView.CurrentCell == null || toDoListView.CurrentCell.RowIndex < 0)
+                {
+                    MessageBox.Show("삭제될 항목을 먼저 선택하세요.");
+                    return;
+                }
+
+                todoList.Rows[toDoListView.CurrentCell.RowIndex].Delete(); //선택 셀 삭제
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Error: " + ex);
+            }
+        }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            //텍스트 박스가 비어있는 경우
+            if (string.IsNullOrWhiteSpace(titleTextBox.Text) || string.IsNullOrWhiteSpace(descriptionTextBox.Text))
+            {
+                MessageBox.Show("제목과 설명이 비어있습니다.");
+                return;
+            }
+
+            if (isEditing)
+            {
+                //edit 선택된 셀을 수정
+                todoList.Rows[toDoListView.CurrentCell.RowIndex]["Title"] = titleTextBox.Text;
+                todoList.Rows[toDoListView.CurrentCell.RowIndex]["Description"] = descriptionTextBox.Text;
+                todoList.Rows[toDoListView.CurrentCell.RowIndex]["Start"] = calendar.SelectionStart.Date;
+                todoList.Rows[toDoListView.CurrentCell.RowIndex]["End"] = calendar.SelectionEnd.Date;
+            }
+            else
+            {
+                //열 추가
+                todoList.Rows.Add(
+                    titleTextBox.Text,
+                    descriptionTextBox.Text,
+                    calendar.SelectionStart.Date,
+                    calendar.SelectionEnd.Date
+                );
+            }
+            //초기화
+            titleTextBox.Text = "";
+            descriptionTextBox.Text = "";
+            calendar.SelectionStart = calendar.TodayDate;
+            calendar.SelectionEnd = calendar.TodayDate;
+            isEditing = false;
+
+        }
+
+        //빈 셀은 체크박스 비활성화
+        private void toDoListView_CellBeginEdit(object sender, DataGridViewCellCancelEventArgs e)
+        {
+            if (toDoListView.Columns[e.ColumnIndex].Name == "IsCompleted")
+            {
+                var taskName = toDoListView.Rows[e.RowIndex].Cells["Title"].Value?.ToString();
+
+                if (string.IsNullOrWhiteSpace(taskName))
+                {
+                    e.Cancel = true;
+                }
+            }
+        }
+
+
+
+    }
+}
